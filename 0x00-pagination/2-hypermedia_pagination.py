611f372c1dbeecc6f08bdbbdace378b9312cef0e
AishaKhalfan <aishakhalfan45@gmail.com>
2023-07-26 00:14:46 +0300

    Hypermedia Pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-module for task 1: Simple pagination
+module for task 2:
+     Hypermedia pagination
 """
 
 import csv
@@ -59,3 +60,32 @@ class Server:
         if start_index > len(data):
             return []
         return data[start_index: end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Get the page of data.
+
+        Args:
+            self: The CSV file name.
+            page: The page number.
+            page_size: The number of items per page.
+        Returns:
+            A dictionary.
+        """
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+        page_size = len(self.get_page(page, page_size))
+        data = self.get_page(page, page_size)
+        total_pages = math.floor(len(self.dataset()) / page_size)
+        next_page = page + 1 if page + 1 < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        metadata = {
+                "page_size": page_size,
+                "page": page,
+                "data": data,
+                "next_page": next_page,
+                "prev_page": prev_page,
+                "total_pages": total_pages
+            }
+
+        return metadata
