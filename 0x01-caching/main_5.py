29dffead5f5a5c9f9b29cf111036bfcb83c71209
AishaKhalfan <aishakhalfan45@gmail.com>
2023-07-27 23:32:08 +0300

    Basic caching

---
+++ 0x01-caching/main_5.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Test
+"""
+import sys
+
+try:
+    BasicCache = __import__('0-basic_cache').BasicCache
+
+    my_cache = BasicCache()
+    my_cache.print_cache()
+    my_cache.put("test1", "myValue")
+    my_cache.print_cache()
+except:
+    print(sys.exc_info()[1])
