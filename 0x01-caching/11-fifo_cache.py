82138eb97f7c3ff6ddcec6620293243e37c91959
AishaKhalfan <aishakhalfan45@gmail.com>
2023-07-30 23:09:05 +0300

    FIFO caching

--- 0x01-caching/11-fifo_cache.py
+++ 0x01-caching/11-fifo_cache.py
@@ -17,4 +17,22 @@ class FIFOCache(BaseCaching):
         """Put function definition"""
         if key and item:
             self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                # Get the first item (FIFO) and its key to be discarded
+                oldest_key = next(iter(self.cache_data))
+                # Discard the oldest item   
+                print(f"DISCARD:{oldest_key}")
+                del(self.cache_data[oldest_key])
+
+    def get(self, key):
+        """Get value in self.cache_data"""
+        if key is None or key not in self.cache_data:
+            return None
+        else:
+            return self.cahe_data[key]
+        # if key is not None:
+        #     for k, value in self.cache_data.items():
+        #         if k == key:
+        #             return value
+        # return None
         
