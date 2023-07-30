475a739e0f9e80fc8fcf0662ddea50aafdac7925
AishaKhalfan <aishakhalfan45@gmail.com>
2023-07-31 00:17:47 +0300

    LIFO CACHING

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -10,15 +10,17 @@ class LIFOCache(BaseCaching):
     """LIFOCache that inherits from BaseCaching"""
     def __init__(self) -> None:
         super().__init__()
+        self.last = ""
 
     def put(self, key, item):
         """PUT FUNCTION"""
         if key and item:
+            self.cache_data[key] = item
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
                 lastItem = list(self.cache_data.keys())[-1]
                 print(f"DISCARD:{lastItem}")
                 del(self.cache_data[lastItem])
-            self.cache_data[key] = item
+            self.last = key
 
     def get(self, key):
         """Get function"""
