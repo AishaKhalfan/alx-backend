78d8410e03db3980bbeca8c9e9f71c3f27e2a1c2
AishaKhalfan <aishakhalfan45@gmail.com>
2023-08-08 17:14:50 +0300

    locale

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -23,6 +23,7 @@ babel = Babel(app)
 def get_locale():
     """Locale selector."""
     if request.args.get('locale') in Config.LANGUAGES:
+        print(locale)
         return request.args.get('locale')
 
     return request.accept_languages.best_match(app.config['LANGUAGES'])
