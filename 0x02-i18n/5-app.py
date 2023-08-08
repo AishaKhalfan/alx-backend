696ba3e2382ad56115aaadee2355063b36875c50
AishaKhalfan <aishakhalfan45@gmail.com>
2023-08-08 22:32:22 +0300

    Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,43 +1,74 @@
 #!/usr/bin/env python3
-""" Mock logging in"""
-from flask import Flask, render_template, request
-from flask_babel import Babel, _
+"""Force locale with URL parameter
+"""
+
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
 
 
 class Config(object):
     """
-    This class contains a list of languages
+    Returns:
     """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-# configuring the app
+# configuring the flask app
 app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+
+def get_user() -> dict:
+    """
+    returns a user dictionary or None if the ID 
+    cannot be found or if login_as was not passed.
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request():
+    """find a user if any, and set it as a global on flask.g.user"""
+    if get_user():
+        g.user = get_user()
 
 
 @babel.localeselector
 def get_locale():
     """
-    Locale selector.
+    Returns locale
     """
-    if request.args.get('locale') in app.config['LANGUAGES']:
-        return request.args.get('locale')
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        print(locale)
+        return locale
 
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def index():
+    """mocking log in
     """
-    returns the rendered template `4-index.html`
-    """
-    # locale = get_locale()
-    return render_template('4-index.html')
+    return render_template('5-index.html', users=users)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port="5000", host="0.0.0.0", debug=True)
